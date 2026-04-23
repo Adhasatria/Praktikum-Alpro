dcb732eef3e72ce58c498d0c73803b9303036d40
adhasatria <adhasatria24@gmail.com>
2026-04-23 11:37:42 +0800

    membuat fungsi rekursif

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int power(int x, int y){
+    if (y==0){
+        return 1;
+    }
+    return x * power(x,y-1);
+}
+
 int main (){
-    cout<<"hello world"<<endl;
+    int basis;
+    int pangkat;
+    cin>>basis;
+    cin>>pangkat;
+
+    cout<<power(basis,pangkat);
     return 0;
 }
